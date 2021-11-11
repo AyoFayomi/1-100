9d6974e30ec00796a11a87ef8d737e3db71126b1
Fayomi <storario2@Gmail.com>
2021-11-11 15:27:28 +0100

    adding 2nd project

---
+++ second/app.js
@@ -0,0 +1,31 @@
+//initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        }
+        else if (styles.contains("increase")) {
+            count++;
+        } 
+        else{
+            count=0
+        }
+        value.textContent = count;
+        if(count > 0){
+            value.style.color = "green";
+        }
+        else if(count < 0){
+            value.style.color = "red";
+        }
+        if( count === 0){
+            value.style.color = "#222";
+        }
+    })
+});
