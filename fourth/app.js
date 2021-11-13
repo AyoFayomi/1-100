87648dd12deea805592d41a31257bb5d34316171
Fayomi <storario2@Gmail.com>
2021-11-13 14:29:19 +0100

    fourth day

---
+++ fourth/app.js
@@ -0,0 +1,14 @@
+
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener('click', () => {
+    // if (links.classList.contains("show-links")){
+    //     links.classList.remove('show-links');
+    // }
+    // else{ 
+    //     links.classList.add("show-links");
+    // }
+    links.classList.toggle("show-links");
+})
