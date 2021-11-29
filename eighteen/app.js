f0135440f6aeef5280978231226a294125ea3a43
AyoFayomi <storario2@gmail.com>
2021-11-29 19:54:48 +0100

    day 18

---
+++ eighteen/app.js
@@ -0,0 +1,8 @@
+new Swiper('.swiper-container',{
+    speed : 400,
+    spaceBetween : 100,
+    navigation : {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+})
